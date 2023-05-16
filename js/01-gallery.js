872c54cc0eb74141112355f513279af53a920b66
Enma04 <programmer1ebg@gmail.com>
2023-05-16 00:53:36 -0500

    Tarea-1 completada!

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,15 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = document.querySelector(".gallery");
+
+galleryItems.forEach( item => {
+    let li = document.createElement("li");
+    gallery.append(li);
+
+    let img = document.createElement("img");
+    img.src = item.preview;
+    li.append(img);
+});
+
 console.log(galleryItems);
