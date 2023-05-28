5015cc569967462711459eaa43260ace26c5b3de
Enma04 <programmer1ebg@gmail.com>
2023-05-28 09:36:53 -0500

    Se creó la galería desde código

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,15 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const gallery = document.querySelector(".gallery");
+import * as basicLightbox from 'basicLightbox.min.js';
 
-galleryItems.forEach( item => {
-    let li = document.createElement("li");
-    gallery.append(li);
+const instance = basicLightbox.create(`
+    <div class="modal">
+        <p>
+            Your first lightbox with just a few lines of code.
+            Yes, it's really that simple.
+        </p>
+    </div>
+`);
 
-    let img = document.createElement("img");
-    img.src = item.preview;
-    li.append(img);
+const galleryList = document.querySelector(".gallery");
+const galleryHTML = galleryItems.map( (element) => 
+    `<li class="gallery__item">
+        <a class="gallery__link" href="${element.original}">
+            <img
+                class="gallery__image"
+                src="${element.preview}"
+                data-source="large-image.jpg"
+                alt="${element.description}"
+            />
+        </a>
+    </li>`).join("");
+
+console.log(galleryHTML);
+galleryList.insertAdjacentHTML('afterbegin', galleryHTML);
+
+let ligthBox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
 });
 
-console.log(galleryItems);
