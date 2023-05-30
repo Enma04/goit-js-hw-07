a4a441df95e61f5f1a6f42b95b977a83c5756e47
Enma04 <programmer1ebg@gmail.com>
2023-05-30 17:55:48 -0500

    Tarea 2 v1.0

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,48 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+//--------------------------------------------------------------------------------
+//------------------------ VARIABLES ---------------------------------------------
+
+const galleryList = document.querySelector(".gallery");
+
+const galleryHTML = galleryItems.map( (element) => 
+    `<li class="gallery__item">
+        <a class="gallery__link" href="${element.original}">
+            <img class="gallery__image" src=" ${element.preview} " alt="${element.description}" />
+        </a>
+    </li>`).join("");
+
+var lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: "alt",
+    captionDealy: 250,
+});
+
+
+//--------------------------------------------------------------------------------
+//------------------------ FUNCIONES ---------------------------------------------
+
+function imgSelected(event) {
+
+    event.preventDefault();
+
+    if(event.target.nodeName !== "IMG") {
+        console.log("No es una imágen!");
+        return;
+    }
+    else {
+        lightbox.on('show.simplelightbox', function (event) {
+            event.open();
+            console.log("Estoy en el lightbox");
+        });
+    }
+}
+
+
+//--------------------------------------------------------------------------------
+//------------------- IMPLEMENTACION ---------------------------------------------
+
+galleryList.insertAdjacentHTML('afterbegin', galleryHTML);
+
+galleryList.addEventListener("click", imgSelected);
