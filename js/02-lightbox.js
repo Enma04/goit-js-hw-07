12d2ddba8f5c9f7e351c1b9e6dc04ac09be27aeb
Enma04 <programmer1ebg@gmail.com>
2023-05-30 19:39:03 -0500

    Tarea 2 v1.1 (Terminada)

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,48 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-
-//--------------------------------------------------------------------------------
-//------------------------ VARIABLES ---------------------------------------------
-
 const galleryList = document.querySelector(".gallery");
 
-const galleryHTML = galleryItems.map( (element) => 
+const galleryHTML = galleryItems.map( ( { original, preview, description } ) => 
     `<li class="gallery__item">
-        <a class="gallery__link" href="${element.original}">
-            <img class="gallery__image" src=" ${element.preview} " alt="${element.description}" />
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src=" ${preview} " alt="${description}" />
         </a>
     </li>`).join("");
 
-var lightbox = new SimpleLightbox('.gallery a', { 
-    captionsData: "alt",
-    captionDealy: 250,
-});
-
-
-//--------------------------------------------------------------------------------
-//------------------------ FUNCIONES ---------------------------------------------
-
-function imgSelected(event) {
-
-    event.preventDefault();
-
-    if(event.target.nodeName !== "IMG") {
-        console.log("No es una imágen!");
-        return;
-    }
-    else {
-        lightbox.on('show.simplelightbox', function (event) {
-            event.open();
-            console.log("Estoy en el lightbox");
-        });
-    }
-}
-
-
-//--------------------------------------------------------------------------------
-//------------------- IMPLEMENTACION ---------------------------------------------
-
 galleryList.insertAdjacentHTML('afterbegin', galleryHTML);
 
-galleryList.addEventListener("click", imgSelected);
+var lightbox = new SimpleLightbox( '.gallery a', { captionsData: "alt", captionDealy: 250} );
+
+lightbox.on('show.simplelightbox', (e) => {
+    e.preventDefault();
+    console.log("Estoy en el lightbox!");
+});
+
